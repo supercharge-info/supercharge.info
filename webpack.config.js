50c14ba10cdf6a7f97498f5901fd61403121c84d
Paul K VA <paulkva@gmail.com>
2023-02-19 10:18:48 -0500

    Remove extra reference to CSS extract plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 const webpack = require('webpack');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
